66d87127458b39a1d4ae4d8a8c17f21abf40c88a
themadhukiran <pmadhukiran96@gmail.com>
2016-01-28 13:08:34 +0530

    change google analytics code

--- controllers/PositionFormController.js
+++ controllers/PositionFormController.js
@@ -1,53 +1,60 @@
 (function () {
     var app = angular.module('Vnb');
 
-    app.controller('PositionFormController', ['StateService','$scope', '$modalInstance','VnbRestangular','positionData','allCorners',
-        function(StateService,$scope, $modalInstance, VnbRestangular, positionData, allCorners){
+    app.controller('PositionFormController', [
+        'StateService',
+        '$scope',
+        '$modalInstance',
+        'VnbRestangular',
+        'positionData',
+        'allCorners',
+        function (StateService, $scope, $modalInstance, VnbRestangular, positionData, allCorners) {
             var posFormCtrl = this;
             var emptyPosition = {
                 name: '',
-                managing_corners:[],
-                editing_corners:[],
-                users:[]
+                managing_corners: [],
+                editing_corners: [],
+                users: []
             };
             posFormCtrl.allCorners = allCorners;
             posFormCtrl.addUsersCollapsed = true;
 
-            posFormCtrl.close = function(){
+            posFormCtrl.close = function () {
                 StateService.showToast('Position saved');
+                StateService.getUserData(true);
                 $modalInstance.close();
             };
 
-            posFormCtrl.showError = function(){
+            posFormCtrl.showError = function () {
 
             };
 
-            posFormCtrl.post = function(){
+            posFormCtrl.post = function () {
                 VnbRestangular.all('positions')
                     .customPOST(posFormCtrl.position, posFormCtrl.postTo)
                     .then(posFormCtrl.close, posFormCtrl.showError);
             };
-            posFormCtrl.addUser = function(user){
+            posFormCtrl.addUser = function (user) {
                 var i = posFormCtrl.position.users.indexOf(user);
-                if(i === -1) {
+                if (i === -1) {
                     posFormCtrl.position.users.push(user);
                     posFormCtrl.addUsersCollapsed = true;
                 }
             };
-            posFormCtrl.removeUser = function(user){
+            posFormCtrl.removeUser = function (user) {
                 var i = posFormCtrl.position.users.indexOf(user);
-                if(i !== -1){
-                    posFormCtrl.position.users.splice(i,1);
+                if (i !== -1) {
+                    posFormCtrl.position.users.splice(i, 1);
                 }
             };
 
-            posFormCtrl.showAddUser = function(){
+            posFormCtrl.showAddUser = function () {
                 posFormCtrl.addUsersCollapsed = false;
             };
 
-            function initialise(){
+            function initialise() {
                 posFormCtrl.loading = true;
-                if(positionData){
+                if (positionData) {
                     posFormCtrl.modalTitle = 'Edit Position';
                     posFormCtrl.submitText = 'Edit';
                     posFormCtrl.postTo = 'edit';
@@ -56,18 +63,18 @@
                     StateService.startLoading();
                     //Request for position data
                     VnbRestangular.all('positions')
-                        .customPOST({id:positionData.id}, 'view')
-                        .then(function(data){
+                        .customPOST({id: positionData.id}, 'view')
+                        .then(function (data) {
                             StateService.stopLoading();
                             posFormCtrl.position = data;
-                        }, function(){
+                        }, function () {
                             StateService.stopLoading();
                             StateService.showToast('Sorry some error occurred. Please try again');
                         });
 
                     posFormCtrl.loading = false;
                 }
-                else{
+                else {
                     posFormCtrl.modalTitle = 'Add Position';
                     posFormCtrl.submitText = 'Add';
                     posFormCtrl.postTo = 'add';
@@ -79,5 +86,5 @@
             }
 
             initialise();
-    }]);
+        }]);
 })();
